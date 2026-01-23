3562a4a415cbdf5a0347a853420223816b44cec6
DastishF <ismailov.miras210794@gmail.com>
2026-01-23 20:07:56 +0500

    Secure: move firebase keys to environment variables

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -5,12 +5,12 @@ import { collection, query, where, getDocs } from "firebase/firestore";
 
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyC5-sjGTuwPPsFTiPfic7WURPIgrAB_DA4",
-  authDomain: "atriumtasks.firebaseapp.com",
-  projectId: "atriumtasks",
-  storageBucket: "atriumtasks.firebasestorage.app",
-  messagingSenderId: "763426705770",
-  appId: "1:763426705770:web:fb462d361f0fb4a7965513",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // ✅ ВАЖНО: экспортируем app
